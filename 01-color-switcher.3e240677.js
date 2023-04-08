d7684270d00f43a80e18b8166b4bade95106fb8e
Adelina515 <117230185+Adelina515@users.noreply.github.com>
2023-04-08 21:46:14 +0000

    Deploying to gh-pages from @ Adelina515/goit-js-hw-09@42f1e621cbe2d47043621508acb839c3b5afbba5 🚀

---
+++ 01-color-switcher.3e240677.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("data-start"),stopBtn:document.querySelector("data-stop")};t.startBtn.addEventListener("click",(function(t){})),t.stopBtn.addEventListener("click",(function(t){}))}();
+//# sourceMappingURL=01-color-switcher.3e240677.js.map
