079623472428715a29ada058a012d9b4643ded51
Adelina515 <adelgodlevska@gmail.com>
2023-04-09 23:31:43 +0300

    add data

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,15 +11,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 } */
-const refs = {
-    startBtn: document.querySelector('data-start'),
-    stopBtn: document.querySelector('data-stop'),
-    body: document.querySelector('body'),
-};
-refs.startBtn.addEventListener('click', handleBtnStart);
-refs.stopBtn.addEventListener('click', handleBtnStop);
 
-function handleBtnStart(ev) {
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener("click", handleBtnStart);
+refs.stopBtn.addEventListener("click", handleBtnStop);
+
+function handleBtnStart (ev) {
   
   refs.body = setInterval(getRandomHexColor, 1000);
 
