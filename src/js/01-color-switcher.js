0817eb949c37e95b2ff9de217682e4c20876a407
Adelina515 <adelgodlevska@gmail.com>
2023-04-09 23:06:27 +0300

    add fun

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -14,12 +14,18 @@ function getRandomHexColor() {
 const refs = {
     startBtn: document.querySelector('data-start'),
     stopBtn: document.querySelector('data-stop'),
+    body: document.querySelector('body'),
 };
 refs.startBtn.addEventListener('click', handleBtnStart);
 refs.stopBtn.addEventListener('click', handleBtnStop);
 
 function handleBtnStart(ev) {
-   
+  
+  refs.body = setInterval(getRandomHexColor, 1000);
+
+  function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 };
 
 
